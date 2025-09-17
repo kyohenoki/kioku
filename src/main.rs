541198b0042b9cb9e2a1253d0fbca13af8872da9
kyohei <kyohenoki@gmail.com>
2025-09-17 14:30:11 +0900

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("kioqu");
+use std::error::Error;
+use reqwest;
+use scraper::{Html, Selector};
+
+// URLのタイトルを取得してみる
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://archive.org";
+    let html = reqwest::get(url).await?.text().await?;
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse("title").unwrap();
+    let title = document.select(&selector).next().map(|e| e.inner_html()).unwrap();
+    println!("{}", title);
+    Ok(())
 }
